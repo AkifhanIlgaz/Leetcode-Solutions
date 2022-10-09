a8fe4db5365d961711873d9d3c3491b9983bca12
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-10-09 19:47:54 +0300

    Update

---
+++ Data Structures I/Day 4/Pascal's-Triangle.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = (numRows) => {
+  let result = [[1]];
+
+  for (let i = 0; i < numRows - 1; i++) {
+    let temp = [0].concat(result.at(-1));
+    temp.push(0);
+
+    let row = [];
+
+    for (let j = 0; j < result.at(-1).length + 1; j++) {
+      row[j] = temp[j] + temp[j + 1];
+    }
+
+    result.push(row);
+  }
+
+  return result;
+};
+console.log(generate(5
+    ));
+
---
+++ Data Structures I/Day 4/Reshape-The-Matrix.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+const matrixReshape = (mat, r, c) => {
+  if (mat.length * mat[0].length !== r * c) {
+    return mat;
+  }
+
+  let arr = [];
+  mat.map((array) => {
+    arr.push(...array);
+  });
+
+  let result = new Array();
+  for (let i = 0; i < r; i++) {
+    result.push(new Array(c));
+  }
+
+  let k = 0;
+
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      result[i][j] = arr[k];
+      k++;
+    }
+  }
+
+  return result;
+};
