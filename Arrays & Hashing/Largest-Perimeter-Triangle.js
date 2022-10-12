0c2f00c268140903b0845acf2fe6a8df58a9a8ab
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-10-12 12:28:30 +0300

    Create Largest-Perimeter-Triangle.js

---
+++ Arrays & Hashing/Largest-Perimeter-Triangle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const largestPerimeter = (nums) => {
+  nums.sort((a, b) => a - b);
+  nums.reverse();
+
+  let maxPerimeter = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] < nums[i + 1] + nums[i + 2]) {
+      maxPerimeter = Math.max(
+        maxPerimeter,
+        nums[i] + nums[i + 1] + nums[i + 2]
+      );
+      break;
+    }
+  }
+
+  return maxPerimeter ? maxPerimeter : 0;
+};
+
+console.log(largestPerimeter([1, 2, 1]));
