7c0a7401fbf7958ba265ac4283404194caa3974e
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-10-29 17:00:54 +0300

    Create Earliest-Possible-Day-Of-Full-Bloom.js

---
+++ Javascript/Arrays & Hashing/Earliest-Possible-Day-Of-Full-Bloom.js
@@ -0,0 +1,33 @@
+class Seed {
+  constructor(plantTime, growTime) {
+    this.plantTime = plantTime;
+    this.growTime = growTime;
+  }
+}
+
+/**
+ * @param {number[]} plantTime
+ * @param {number[]} growTime
+ * @return {number}
+ */
+const earliestFullBloom = (plantTime, growTime) => {
+  const plant = [];
+  for (let i = 0; i < plantTime.length; i++) {
+    const seed = new Seed(plantTime[i], growTime[i]);
+    plant.push(seed);
+  }
+
+  plant.sort((a, b) => b.growTime - a.growTime);
+
+  console.log(plant);
+  let time = 0;
+  let max = 0;
+
+  for (let i = 0; i < plantTime.length; i++) {
+    time += plant[i].plantTime;
+    max = Math.max(max, time + plant[i].growTime);
+  }
+  return max;
+};
+
+earliestFullBloom([1, 2, 3, 2], [2, 1, 2, 1]);
