1c34ead3ccd41b51ac7e41f9f2be191b206c005b
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-11-07 16:44:16 +0300

    Update

---
+++ Rust/To-Lower-Case.rs
@@ -0,0 +1,16 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        String::from_utf8(
+            s.as_bytes()
+                .iter()
+                .map(|&c| c + if c >= b'A' && c <= b'Z' { 32 } else { 0 })
+                .collect(),
+        )
+        .unwrap()
+
+        // s.to_ascii_lowercase()
+    }
+}
+fn main() {}
