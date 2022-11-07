3f16413a73bd0c886b0a1734eccf88791c4574a4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-11-07 12:23:42 +0300

    Create Maximum-69-Number.js

---
+++ Javascript/Math/Maximum-69-Number.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param {number} num
+ * @return {number}
+ */
+const maximum69Number = (num) => {
+  let temp = num.toString().split("");
+
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] !== "9") {
+      temp[i] = "9";
+      break;
+    }
+  }
+  
+  return +temp.join("");
+
+  // return +num.toString().replace("6", "9");
+};
+
+console.log(maximum69Number(99));
