d29cacd6ec4cd78ca0383747dd4143e543e27deb
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-05 21:11:43 +0300

    update

---
+++ Go/Arrays & Hashing/Average-Salary-Excluding-Minimum-And-Maximum-Salary.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+func main() {
+
+}
+
+func average(salary []int) float64 {
+	var min float64 = float64(salary[0])
+	var max float64 = 0;
+	var sum float64 = 0
+
+	for _, sal := range salary {
+		sum += float64(sal)
+		max = math.Max(float64(max), float64(sal));
+		min = math.Min(min, float64(sal))
+	}
+
+	return (sum - min-max) / float64(len(salary) - 2)
+}
---
+++ Go/Math/Count-Odd-Numbers-In-An-Interval-Range.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+}
+
+func countOdds(low int, high int) int {
+	result := (high - low) / 2
+
+	if low%2 != 0 || high%2 != 0 {
+		result++
+	}
+
+	return result
+}
