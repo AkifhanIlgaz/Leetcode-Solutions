76d48a6fd4650216a808e7aea57ac818c3670f95
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-13 14:41:29 +0300

    Update To-Lower-Case.rs

--- Rust/String/To-Lower-Case.rs
+++ Rust/String/To-Lower-Case.rs
@@ -2,13 +2,17 @@ pub struct Solution {}
 
 impl Solution {
     pub fn to_lower_case(s: String) -> String {
-        String::from_utf8(
-            s.as_bytes()
-                .iter()
-                .map(|&c| c + if c >= b'A' && c <= b'Z' { 32 } else { 0 })
-                .collect(),
-        )
-        .unwrap()
+        // String::from_utf8(
+        //     s.as_bytes()
+        //         .iter()
+        //         .map(|&c| c + if c >= b'A' && c <= b'Z' { 32 } else { 0 })
+        //         .collect(),
+        // )
+        // .unwrap()
+
+        s.chars()
+            .map(|ch| ch.to_ascii_lowercase())
+            .collect::<String>()
 
         // s.to_ascii_lowercase()
     }
