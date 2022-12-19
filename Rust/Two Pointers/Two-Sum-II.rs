0f706f39e88e15c7dd6d9fe8fa8a6f42bfa1c865
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-19 14:28:43 +0300

    Update Two-Sum-II.rs

--- Rust/Two Pointers/Two-Sum-II.rs
+++ Rust/Two Pointers/Two-Sum-II.rs
@@ -1,16 +1,15 @@
 struct Solution;
 
+use std::cmp::Ordering;
+
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
         let (mut left, mut right) = (0, numbers.len() - 1);
         loop {
-            let sum = numbers[left] + numbers[right];
-            if sum < target {
-                left += 1;
-            } else if sum > target {
-                right -= 1;
-            } else {
-                return vec![(left + 1) as i32, (right + 1) as i32];
+            match (numbers[left] + numbers[right]).cmp(&target) {
+                Ordering::Equal => return vec![(left + 1) as i32, (right + 1) as i32],
+                Ordering::Greater => right -= 1,
+                Ordering::Less => left += 1,
             }
         }
     }
