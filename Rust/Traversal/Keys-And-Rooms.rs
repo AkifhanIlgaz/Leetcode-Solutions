2fd69ebc7e28b37ac5e66a9fbb0c5e01c5f66226
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-20 14:34:30 +0300

    Update Keys-And-Rooms.rs

--- Rust/Traversal/Keys-And-Rooms.rs
+++ Rust/Traversal/Keys-And-Rooms.rs
@@ -2,7 +2,6 @@ struct Solution;
 
 impl Solution {
     pub fn can_visit_all_rooms(rooms: Vec<Vec<i32>>) -> bool {
-        let mut count = 1;
         let mut keys: Vec<usize> = vec![0];
         let mut visited: Vec<bool> = vec![false; rooms.len()];
 
@@ -11,10 +10,6 @@ impl Solution {
         while let Some(room) = keys.pop() {
             for key in rooms[room].iter().map(|key| *key as usize) {
                 if !visited[key] {
-                    count += 1;
-                    if count == rooms.len() {
-                        return true;
-                    }
                     visited[key] = true;
                     keys.push(key);
                 }
