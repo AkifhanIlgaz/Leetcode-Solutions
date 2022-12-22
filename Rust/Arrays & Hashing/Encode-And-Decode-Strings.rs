622abb24fbb976064c40797485cf04e936ddf2d9
AkifhanIlgaz <akifhanilgaz@gmail.com>
2022-12-22 13:27:25 +0300

    Update Encode-And-Decode-Strings.rs

--- Rust/Arrays & Hashing/Encode-And-Decode-Strings.rs
+++ Rust/Arrays & Hashing/Encode-And-Decode-Strings.rs
@@ -9,27 +9,21 @@ impl Solution {
 
     pub fn decode(str: String) -> Vec<String> {
         let mut decoded_words = vec![];
-        let str = str.as_bytes().to_vec();
+        let str_as_bytes = str.as_bytes();
         let mut i = 0;
         while i < str.len() {
             let mut j = i;
-            while str[j] != b'#' {
+            while str_as_bytes[j] != b'#' {
                 j += 1;
             }
-            let len: usize = String::from_utf8_lossy(&str[i..j])
-                .to_string()
-                .parse()
-                .unwrap();
+            let len = &str[i..j].to_string().parse().unwrap();
 
-            let word = &str[j + 1..j + len + 1];
-            decoded_words.push(word);
-            i = j + 1 + len
+            let word = &str[j + 1..j + len + 1].to_string();
+            decoded_words.push(word.clone());
+            i = j + 1 + len;
         }
 
         decoded_words
-            .iter()
-            .map(|word| String::from_utf8(word.to_vec()).unwrap())
-            .collect()
     }
 }
 
