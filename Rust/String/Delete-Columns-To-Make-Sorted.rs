02b35d81fe2340494b6f0a21ffebbe33f4ca1b1e
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-01-03 13:23:21 +0300

    Update Delete-Columns-To-Make-Sorted.rs

--- Rust/String/Delete-Columns-To-Make-Sorted.rs
+++ Rust/String/Delete-Columns-To-Make-Sorted.rs
@@ -18,5 +18,23 @@ impl Solution {
 
         count
     }
+
+    /*
+    impl Solution {
+        pub fn min_deletion_size(strs: Vec<String>) -> i32 {
+            let mut count = 0;
+
+            for index in 0..strs[0].len() {
+                for i in 1..strs.len() {
+                    if strs[i].as_bytes()[index] < strs[i - 1].as_bytes()[index] {
+                        count += 1;
+                        break;
+                    }
+                }
+            }
+            count
+        }
+    }
+    */
 }
 fn main() {}
