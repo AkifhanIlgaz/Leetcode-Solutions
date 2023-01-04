8ceeff37b4b6823093ddb628a857a0d382c74312
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-01-04 14:23:58 +0300

    Update Minimum-Rounds-To-Complete-All-Tasks.rs

--- Rust/Arrays & Hashing/Minimum-Rounds-To-Complete-All-Tasks.rs
+++ Rust/Arrays & Hashing/Minimum-Rounds-To-Complete-All-Tasks.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 impl Solution {
     pub fn minimum_rounds(tasks: Vec<i32>) -> i32 {
         let mut map = HashMap::new();
-        let mut res = 0.;
+        let mut res = 0;
 
         for task in tasks {
             *map.entry(task).or_insert(0) += 1;
@@ -16,9 +16,9 @@ impl Solution {
                 return -1;
             }
 
-            res += (count as f64 / 3.).ceil();
-        }
-        res as i32
+            res += (count + 2) / 3; // Math.ceil(a / b) = ( a + b - 1) / b
+        } 
+        res
     }
 }
 fn main() {}
