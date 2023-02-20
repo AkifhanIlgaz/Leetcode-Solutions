d0bbeedf6a4b099241a127178b8d10615927c581
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-20 14:24:52 +0300

    Add Search-Insert-Position

---
+++ Go/Search-Insert-Position.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		middle := (left + right) / 2
+		if nums[middle] == target {
+			return middle
+		} else if nums[middle] < target {
+			left = middle + 1
+		} else {
+			right = middle - 1
+		}
+	}
+
+	return left
+}
