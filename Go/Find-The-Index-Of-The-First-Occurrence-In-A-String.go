9e71af158d70c6fa951178539b66d9c670e00e00
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-06-20 21:04:08 +0300

    refactor go code

--- Go/Find-The-Index-Of-The-First-Occurrence-In-A-String.go
+++ Go/Find-The-Index-Of-The-First-Occurrence-In-A-String.go
@@ -2,12 +2,12 @@ package main
 
 func strStr(haystack string, needle string) int {
 	//  strings.Index(haystack, needle)
+	lenNeedle := len(needle)
+	firstLetter := needle[0]
 
-	for i := 0; i <= len(haystack)-len(needle); i++ {
-		if haystack[i] == needle[0] {
-			if haystack[i:i+len(needle)] == needle {
-				return i
-			}
+	for i := 0; i <= len(haystack)-lenNeedle; i++ {
+		if haystack[i] == firstLetter && haystack[i:i+lenNeedle] == needle {
+			return i
 		}
 	}
 
