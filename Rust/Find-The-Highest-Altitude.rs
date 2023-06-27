cbba3ab139646047cf5ae1dbd8cf383723a7a716
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-06-27 17:25:54 +0300

    solve find the highest altitude for rust

---
+++ Rust/Find-The-Highest-Altitude.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn largest_altitude(gain: Vec<i32>) -> i32 {
+        let mut current_altitude = 0;
+        let mut max_altitude = 0;
+
+        for change in gain {
+            current_altitude += change;
+            max_altitude = std::cmp::max(max_altitude, current_altitude)
+        }
+
+        max_altitude
+    }
+}
