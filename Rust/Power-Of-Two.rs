7dc19845f5e8dd6f3e9328fd9a5f60f4bd23b50c
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-02 16:49:47 +0300

    solve power of two for rust

---
+++ Rust/Power-Of-Two.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        if n == 0 {
+            return false;
+        }
+
+        let mut n = n;
+
+        while n % 2 == 0 {
+            n = n >> 1;
+        }
+
+        n == 1
+    }
+}
