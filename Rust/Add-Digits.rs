765c646989cd922e8e08817c4de9abcb4121a103
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-08 13:43:41 +0300

    solve add digits for rust

--- Rust/Add-Digits.rs
+++ Rust/Add-Digits.rs
@@ -1,1 +1,20 @@
+impl Solution {
+    pub fn add_digits(num: i32) -> i32 {
+        if num < 10 {
+            return num;
+        }
 
+        Solution::add_digits(Solution::sum_of_digits(num))
+    }
+
+    pub fn sum_of_digits(mut num: i32) -> i32 {
+        let mut sum = 0;
+
+        while num > 0 {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        sum
+    }
+}
