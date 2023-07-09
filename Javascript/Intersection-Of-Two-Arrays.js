e9ba6dc1383e7bf4769f12184ad3b99eb5379e34
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-09 22:54:25 +0300

    solve Intersection Of Two Array for javascript

--- Javascript/Intersection-Of-Two-Arrays.js
+++ Javascript/Intersection-Of-Two-Arrays.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-    
+  const numsMap = new Set(nums1);
+  const intersectionOfNums = [];
+
+  for (let num of nums2) {
+    if (numsMap.has(num)) {
+      intersectionOfNums.push(num);
+      numsMap.delete(num);
+    }
+  }
+
+  return intersectionOfNums;
 };
